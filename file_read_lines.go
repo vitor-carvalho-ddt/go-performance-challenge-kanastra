aa77944bffc1d6d221592bdbbe8fb54823a22e3c
vitor-carvalho-ddt <vitor.carvalho@kanastra.com.br>
2025-02-28 17:31:05 -0300

    Filtering a given column by comma (delimiter)  index

--- file_read_lines.go
+++ file_read_lines.go
@@ -6,12 +6,30 @@ import(
 	"os"
 	"path/filepath"
 	"bufio"
+	"strings"
 )
 
 func check(e error){
 	if e != nil {
 		panic(e)
 	}
+}
+
+func IndexOfNth(text string, delimiter rune, nth int) int {
+	s := 0;
+	last_pos := -1;
+    for i := 0; i < nth; i++ {
+        s = strings.IndexRune(text[last_pos+1:], delimiter);
+        if s == -1{break};
+		if i == 0{
+			last_pos+=s+1
+		}else{
+			s+=1
+			last_pos+=s
+		}
+    }
+	fmt.Printf("nth: %d | last_pos: %d\n", nth, last_pos)
+    return last_pos;
 }
 
 func main(){
@@ -40,7 +58,23 @@ func main(){
 			break;
 		}
 		line := scanner.Text()
-		fmt.Println("Line: ", line)
+		// fmt.Println("Line: ", line)
+		delimiter := ';'
+		nth := 16
+
+		pos := IndexOfNth(line, delimiter, nth)
+		pos_next := IndexOfNth(line, delimiter, nth+1)
+		data := ""
+		if pos_next == -1{
+			fmt.Printf("\nLast occurence of delimiter!\n")
+			data = line[pos+1:]
+		}else{
+			data = line[pos+1:pos_next]
+		}
+
+		fmt.Printf("\nFinding nth %c in line: pos == %d\n", delimiter, pos)
+		fmt.Printf("Data:%s\n", data)
+
 		i += 1
 	}
 
